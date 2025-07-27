ecfb5719bcd05cfa24695163ad3f751f90f79c8a
msyuniarto <msyuniarto@gmail.com>
2025-07-27 14:49:42 +0700

    migration table playlists, playlist_songs, playlist_song_activities, dan collaborations

---
+++ migrations/1753601897633_create-table-playlist-song-activities.js
@@ -0,0 +1,53 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('playlist_song_activities', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    playlist_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    song_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    user_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    action: {
+      type: 'VARCHAR(10)',
+      notNull: true,
+    },
+    time: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+
+  // constraint unique
+  pgm.addConstraint('playlist_song_activities', 'unique_playlist_id_song_id_and_user_id', 'UNIQUE(playlist_id, song_id, user_id)');
+
+  // foreign key
+  pgm.addConstraint('playlist_song_activities', 'fk_playlist_song_activities.playlist_id_playlists.id', 'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE');
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('playlist_song_activities');
+};
